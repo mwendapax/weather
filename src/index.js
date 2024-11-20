cac37e57432e85a26d9781a3956037f1e97f1dd3
mwendapax <160007831+mwendapax@users.noreply.github.com>
2024-11-20 12:29:33 +0300

    Add project dependencies

---
+++ src/index.js
@@ -0,0 +1,1 @@
+console.info('webpack: Configuration succesful')
---
+++ webpack.dev.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+const {merge} = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+    mode : 'development',
+    devtool: 'eval-source-map',
+    devServer : {
+        watchFiles : ['./src/template.html']
+    }
+})
